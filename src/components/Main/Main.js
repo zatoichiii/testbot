5933108ee6cb982367cd56a5cc19df0e4bc2c90b
zatoichiii <azovtsevnikita@mail.ru>
2024-08-19 23:42:54 +0300

    test34

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './Main.css';
+import { Link } from 'react-router-dom';
 
 const Main = () => {
   
     return (
       <div>
         Main
+        <Link to="/form">
+          <button>Перейти к форме</button>
+        </Link>
       </div>
     );
   };
