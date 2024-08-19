497e44b9f9f905b71a36be5ac32c51721274c2bd
zatoichiii <azovtsevnikita@mail.ru>
2024-08-19 20:48:22 +0300

    test8

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = (props) => {
     <div className={'header'}>
       <Button onClick={onClose}>Закрыть</Button>
       <span className={'username'}>
-        {user?.username || 'Unknown User'} // Added a default value
+        {user?.username || 'Unknown User'} 
       </span>
     </div>
   );
