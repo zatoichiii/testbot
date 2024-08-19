f6e466cd0ef4cf1233b439e63eee3502393a4190
zatoichiii <azovtsevnikita@mail.ru>
2024-08-19 22:49:51 +0300

    test27

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,31 +1,24 @@
-const tg = window.Telegram?.WebApp;
+const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
-  if (!tg) {
-    throw new Error("Telegram WebApp is not available");
-  }
 
-  const onClose = () => {
-    tg.close();
-  };
-
-  const onToggleButton = () => {
-    const mainButton = tg.MainButton;
-    if (mainButton) {
-      if (mainButton.isVisible) {
-        mainButton.hide();
-      } else {
-        mainButton.show();
-      }
-    } else {
-      console.error("MainButton is not available");
+    const onClose = () => {
+        tg.close()
     }
-  };
 
-  return {
-    onClose,
-    onToggleButton,
-    tg,
-    user: tg.initDataUnsafe?.user || null, 
-  };
+    const onToggleButton = () => {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
+    }
 }
