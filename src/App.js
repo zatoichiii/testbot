c10a951336ccf928076c02cc82b0bb4a6bebac4a
zatoichiii <azovtsevnikita@mail.ru>
2024-08-19 20:33:22 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useTelegram } from './hooks/useTelegram';
 
 function App() {
 
-  const {onToggleButton} = useTelegram();
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
