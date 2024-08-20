fbca7e0f6a6ace44b1d0a3d5b67a3c306769d31b
zatoichiii <azovtsevnikita@mail.ru>
2024-08-20 21:28:56 +0300

    test45

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import './Header.css';
+const botToken = '6419026468:AAFWQzw6LC1JeboeO4SUxdoepDZEZt4aVzI';
 
 const Header = (props) => {
   const { user } = useTelegram(); 
 
+  const profilePhotos = user?.profile_photos;
+  const photoUrl = profilePhotos && profilePhotos.photos[0][0].file_id;
+
   return (
     <div className={'header'}>
-      <img src={user?.profile_photo_url} alt="User Avatar" className={'avatar'} />
+      <img src={`https://api.telegram.org/file/bot${botToken}/${photoUrl}`} alt="User Avatar" className={'avatar'} />
       <span className={'username'}>
         {user?.username || 'Unknown User'} 
       </span>
