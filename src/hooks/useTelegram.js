d82420cd61cfc7ef5544d4e43ba84d237e4cc3a6
zatoichiii <azovtsevnikita@mail.ru>
2024-08-20 20:39:09 +0300

    test36

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,24 +1,27 @@
-const tg = window.Telegram.WebApp;
-
 export function useTelegram() {
-
+    const tg = window.Telegram.WebApp;
+  
+    if (!tg.initDataUnsafe) {
+      tg.ready();
+    }
+  
     const onClose = () => {
-        tg.close()
+      tg.close()
     }
-
+  
     const onToggleButton = () => {
-        if(tg.MainButton.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
+      if(tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+      } else {
+        tg.MainButton.show();
+      }
     }
-
+  
     return {
-        onClose,
-        onToggleButton,
-        tg,
-        user: tg.initDataUnsafe?.user,
-        queryId: tg.initDataUnsafe?.query_id,
+      onClose,
+      onToggleButton,
+      tg,
+      user: tg.initDataUnsafe?.user,
+      queryId: tg.initDataUnsafe?.query_id,
     }
-}
+  }
