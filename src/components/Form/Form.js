311c5b1b5709607414cb235e76b8acbdafee1dd3
zatoichiii <azovtsevnikita@mail.ru>
2024-08-20 21:01:38 +0300

    test41

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,93 +1,91 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css';
-import { useTelegram } from "../../hooks/useTelegram";
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-  const [userName, setUserName] = useState('');
-  const [userPoints, setUserPoints] = useState('');
-  const [userMessage, setUserMessage] = useState('');
-  const [publicationType, setPublicationType] = useState('public');
-  const { tg } = useTelegram();
+    const [userName, setUserName] = useState('');
+    const [userPoints, setUserPoints] = useState('');
+    const [userMessage, setUserMessage] = useState('');
+    const [publicationType, setPublicationType] = useState('public');
+    const {tg} = useTelegram();
 
-  const onSendData = useCallback(() => {
-    if (tg) {
-      const data = {
-        userName,
-        userPoints,
-        userMessage,
-        publicationType
-      };
-      tg.sendData(JSON.stringify(data));
+    const onSendData = useCallback(() => {
+        const data = {
+            userName,
+            userPoints,
+            userMessage,
+            publicationType
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [userName, userPoints, userMessage, publicationType, tg]) 
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData, tg]) 
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить спасибо'
+        })
+    }, [tg.MainButton]) 
+
+    useEffect(() => {
+        if(!userPoints || !userName) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [userName, userPoints, tg.MainButton])
+
+    const onChangeUserName = (e) => {
+        setUserName(e.target.value)
     }
-  }, [userName, userPoints, userMessage, publicationType, tg]);
 
-  useEffect(() => {
-    if (tg) {
-      tg.MainButton.hide();
+    const onChangeUserPoints = (e) => {
+        setUserPoints(e.target.value)
     }
-  }, [tg && tg.MainButton]);
 
-  useEffect(() => {
-    if (tg) {
-      if (!userPoints || !userName) {
-        tg.MainButton.hide();
-      } else {
-        tg.MainButton.show();
-      }
+    const onChangeUserMessage = (e) => {
+        setUserMessage(e.target.value)
     }
-  }, [userName, userPoints, tg && tg.MainButton]);
 
-  if (!tg) {
-    return <div>Telegram Web App is not initialized</div>;
-  }
+    const onChangePublicationType = (e) => {
+        setPublicationType(e.target.value)
+    }
 
-  const onChangeUserName = (e) => {
-    setUserName(e.target.value);
-  };
-
-  const onChangeUserPoints = (e) => {
-    setUserPoints(e.target.value);
-  };
-
-  const onChangeUserMessage = (e) => {
-    setUserMessage(e.target.value);
-  };
-
-  const onChangePublicationType = (e) => {
-    setPublicationType(e.target.value);
-  };
-
-  return (
-    <div className={"form"}>
-      <h3>Кому вы хотите отдать спасибо?</h3>
-      <input
-        className={'input'}
-        type="text"
-        placeholder={'Имя'}
-        value={userName}
-        onChange={onChangeUserName}
-      />
-      <input
-        className={'input'}
-        type="text"
-        placeholder={'Баллы'}
-        value={userPoints}
-        onChange={onChangeUserPoints}
-      />
-      <input
-        className={'input'}
-        type="text"
-        placeholder={'Сообщение'}
-        value={userMessage}
-        onChange={onChangeUserMessage}
-      />
-      <select value={publicationType} onChange={onChangePublicationType} className={'select'}>
-        <option value={'public'}>Публично</option>
-        <option value={'anonym'}>Анонимно</option>
-      </select>
-      <button onClick={onSendData}>Отправить спасибо</button>
-    </div>
-  );
+    return (
+        <div className={"form"}>
+            <h3>Кому вы хотите отдать спасибо?</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Имя'}
+                value={userName}
+                onChange={onChangeUserName}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Баллы'}
+                value={userPoints}
+                onChange={onChangeUserPoints}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Сообщение'}
+                value={userMessage}
+                onChange={onChangeUserMessage}
+            />
+            <select value={publicationType} onChange={onChangePublicationType} className={'select'}>
+                <option value={'public'}>Публично</option>
+                <option value={'anonym'}>Анонимно</option>
+            </select>
+        </div>
+    );
 };
 
 export default Form;
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,43 +1,24 @@
-import { useState, useEffect } from 'react';
-
+const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
-    const tg = window.Telegram.WebApp;
-    const [isReady, setIsReady] = useState(false);
-  
-    useEffect(() => {
-      if (!tg.initDataUnsafe) {
-        tg.ready(() => {
-          setIsReady(true);
-        });
-      } else {
-        setIsReady(true);
-      }
-    }, [tg]);
-  
-    if (!isReady) {
-      return {
-        error: 'Telegram Web App is not initialized',
-      };
-    }
-  
+
     const onClose = () => {
-      tg.close()
+        tg.close()
     }
-  
+
     const onToggleButton = () => {
-      if(tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-      } else {
-        tg.MainButton.show();
-      }
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
     }
-  
+
     return {
-      onClose,
-      onToggleButton,
-      tg,
-      user: tg.initDataUnsafe.user,
-      queryId: tg.initDataUnsafe.query_id,
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     }
-  }
+}
